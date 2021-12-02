9590ca5b91ec745fbe7114d292de0608faacf8ae
Rohan Singh <rohansingh94117@gmail.com>
2021-12-03 01:01:39 +0530

    Part 2 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,8 +3,11 @@ fun main() {
         return input.windowed(2).count { (a, b) -> a < b }
     }
 
+    //solved with the help of kotlin youtube channel
     fun part2(input: List<Int>): Int {
-        return input.size
+        return input.windowed(4).count {
+            it[0] < it[3]
+        }
     }
 
 
